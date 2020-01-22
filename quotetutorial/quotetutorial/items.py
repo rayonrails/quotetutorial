39b3698cacd0281e969f8fc6887d519bafb3dd6b
Ray Rogers <ray@rayonrails.com>
2020-01-22 12:49:53 -0500

    Start of Lesson 13 | Storing in JSON, XML and CSV

--- quotetutorial/quotetutorial/items.py
+++ quotetutorial/quotetutorial/items.py
@@ -10,5 +10,6 @@ import scrapy
 
 class QuotetutorialItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    title = scrapy.Field()
+    author = scrapy.Field()
+    tag = scrapy.Field()
--- quotetutorial/quotetutorial/spiders/quotes_spider.py
+++ quotetutorial/quotetutorial/spiders/quotes_spider.py
@@ -1,4 +1,5 @@
 import scrapy
+from ..items import QuotetutorialItem
 
 
 class QuoteSpider(scrapy.Spider):
@@ -6,14 +7,16 @@ class QuoteSpider(scrapy.Spider):
     start_urls = ['http://quotes.toscrape.com/']
 
     def parse(self, response):
+        items = QuotetutorialItem()
+
         all_div_quotes = response.css('div.quote')
 
         for quotes in all_div_quotes:
             title = quotes.css('span.text::text').extract()
             author = quotes.css('small.author::text').extract()
             tag = quotes.css('.tag::text').extract()
-            yield {
-                'title': title,
-                'author': author,
-                'tag': tag
-            }
+            items['title'] = title
+            items['author'] = author
+            items['tag'] = tag
+
+            yield items
